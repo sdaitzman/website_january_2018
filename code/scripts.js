8013c3fbe001e20eedef0015f9903aa8c5ffa968
Sam Daitzman <sam@daitzman.com>
2018-04-14 08:44:23 -0400

    pre-typer setup

--- code/scripts.js
+++ code/scripts.js
@@ -1,3 +1,17 @@
-$(function () {
-  // swal('hi')
+$(document).ready(function() {
+
+	$.typer.options = {
+		highlightSpeed: 100,
+		typeSpeed: 200,
+		clearDelay: 500,
+		typeDelay: 200,
+		clearOnHighlight: false,
+		typerDataAttr: 'data-typer-targets',
+		typerInterval: 3000
+	}
+	
+	$('[data-typer-targets]').typer()
+
+	$('body').toggleClass('done')
+
 })
